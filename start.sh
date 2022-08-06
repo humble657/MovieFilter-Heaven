4c9adae25958546dd0f8247cab85d6b95a09f632
humble657 <108530156+humble657@users.noreply.github.com>
2022-08-06 10:17:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Xdecoy1/MovieFilter-tom.git /EvaMaria
+  git clone https://github.com/humble657/MovieFilter-Heaven.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
